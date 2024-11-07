4487841ccfe9a4eb8e3218eb1d51cd389d03596b
David Faveris <david.faveris@dgfip.finances.gouv.fr>
2024-11-07 18:11:43 +0100

    organ

---
+++ organism-exp/README.md
@@ -0,0 +1,6 @@
+# revoir nexttick
+https://nodejs.org/en/learn/asynchronous-work/understanding-processnexttick
+
+# inbox
+- https://stackoverflow.com/questions/13698043/observe-file-changes-with-node-js
+- https://www.npmjs.com/package/chokidar
--- organism-exp/test.js
+++ organism-exp/test.js
@@ -11,4 +11,5 @@ let second = new Organ({name: "deux", autostart: true})
 // overwritting functions _start() / _step() / _stop()
 second._stop = async function(){
   console.log("THIS is the end", this.options.name)
+  this._inbox_manager("stop")
 }
